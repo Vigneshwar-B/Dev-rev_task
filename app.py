f30d52572bcd27c205e24ac92f392ca38500b37a
Vigneshwar <104056311+Vigneshwar-B@users.noreply.github.com>
2023-07-07 17:06:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = "vigneshwar"  # Set your secret key for session management
 
 # Configure PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vigneshwar:OlmCvOoTmhP2IUruhWNgib9chYC1BsQQ@dpg-cijs4ltph6euh7hqi7g0-a.oregon-postgres.render.com/demo_35ax'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vigneshwar:OlmCvOoTmhP2IUruhWNgib9chYC1BsQQ@dpg-cijs4ltph6euh7hqi7g0-a.oregon-postgres.render.com/demo_35ax'
 db = SQLAlchemy(app)
 
 from routes import *
